ca68e42a7edab09578ff6e68ed4b583dbc62fcc6
Maxwell Foley <maxwellfoley@wustl.edu>
2018-01-22 22:51:13 -0600

    displaying and writing image working

--- MaxFoleyRender/Color.h
+++ MaxFoleyRender/Color.h
@@ -9,22 +9,39 @@
 #ifndef Color_h
 #define Color_h
 
-class MFR::Color {
-	public:
-		float r;
-		float g;
-		float b;
-		float a;
-	
-		MFR::Color(float _r, float _g, float _b, float _a)
-		{
-			r = _r; g = _g; b = _b; a = _a;
-		}
+namespace MFR {
+	class Color {
+		public:
+			float r;
+			float g;
+			float b;
+			float a;
+		
+			Color(void)
+			{/*
+				a = 1.0;
+				r = 0.0;
+				g = 0.0;
+				b = 0.0;				
+			*/
+				a = 1.0;
+				r = 0.0;
+				g = 0.0;
+				b = 0.0;
+			}
+			
+			Color(float _r, float _g, float _b, float _a)
+			{
+				r = _r; g = _g; b = _b; a = _a;
+			}
 
-		MFR::Color(float _r, float _g, float _b)
-		{
-			r = _r; g = _g; b = _b; a = 1.0;
-		}
+			Color(float _r, float _g, float _b)
+			{
+				r = _r; g = _g; b = _b; a = 1.0;
+			}
+		
+
+	};
 }
 
 #endif /* Color_h */
--- MaxFoleyRender/MaxFoleyRender/main.cpp
+++ MaxFoleyRender/MaxFoleyRender/main.cpp
@@ -8,28 +8,111 @@
 
 #include <stdio.h>
 #include "Vector.h"
+#include "Color.h"
 #include "SDL2/SDL.h"
+#include <pngwriter.h>
+
+void calculateImage(MFR::Color * buf, int width, int height)
+{
+	for(int i = 0; i < width*height; i++)
+	{
+		buf[i] = MFR::Color(1.0,0.0,0.0);
+	}
+}
+
+void writeImageToFile(MFR::Color * buf, char * filename, int width, int height)
+{
+   pngwriter png(width,height,0,filename);
+   for(int i = 0; i < width*height; i++)
+	{
+     	png.plot(i%width,i/width, buf[i].r*255, buf[i].g*255, buf[i].b*255);
+	}
+     std::cout << " just making sure we actually did it " << std::endl;
+   png.close();
+}
+
+SDL_Texture * getImageTexture(char * filename, SDL_Window * win, SDL_Renderer * ren)
+{
+	SDL_Surface *bmp = SDL_LoadBMP(filename);
+	if (bmp == nullptr){
+		SDL_DestroyRenderer(ren);
+		SDL_DestroyWindow(win);
+		std::cout << "SDL_LoadBMP Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return nullptr;
+	}
+	
+	SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, bmp);
+	SDL_FreeSurface(bmp);
+	if (tex == nullptr){
+		SDL_DestroyRenderer(ren);
+		SDL_DestroyWindow(win);
+		std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return nullptr;
+	}
+	return tex;
+}
+
+SDL_Texture * getBlankTexture(SDL_Window * win, SDL_Renderer * ren)
+{
+
+ 	//The final texture
+	SDL_Texture* tex = SDL_CreateTexture(ren, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, 640, 480);
+
+	if (tex == nullptr){
+		SDL_DestroyRenderer(ren);
+		SDL_DestroyWindow(win);
+		std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return nullptr;
+	}
+	return tex;
+}
+
+void writeColorBufferToTexture(SDL_Texture * tex, MFR::Color * buf)
+{
+	Uint32* pixels = nullptr;
+	int pitch = 0;
+	Uint32 format;
+	
+	// Get the size of the texture.
+	int w, h;
+	SDL_QueryTexture(tex, &format, nullptr, &w, &h);
+	
+	// Now let's make our "pixels" pointer point to the texture data.
+	//will return zero if success, another number on failure
+	if (SDL_LockTexture(tex, nullptr, (void**)&pixels, &pitch))
+	{
+		std::cout << "Locking texture failed: " << SDL_GetError() << std::endl;
+	}
+	
+	//copy over the pixel buffer
+	for(int i = 0; i < w*h; i++)
+	{
+		std::cout << i <<  sizeof(pixels[i]) << std::endl;
+		
+		Uint32 color = 0;
+		Uint32 r = buf[i].r*255;
+		Uint32 g = buf[i].g*255;
+		Uint32 b = buf[i].b*255;
+		Uint32 a = buf[i].a*255;
+		color = (r << 24) + (g << 16) + (b << 8) + a;
+		pixels[i] = color;
+	}
+	
+	SDL_UnlockTexture(tex);
+
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
     printf("Hello, World!\n");
-	MFR::Vector v = MFR::Vector(3,3,3);
-	MFR::Vector v2 = MFR::Vector(1,2,3);
-	MFR::Vector v3 = v / v2;
-	
-	std::cout << v.toString() << std::endl;
-	std::cout << v3.toString() << std::endl;
-	
-	MFR::Vector v3unit = v3.unit();
-	float length = v3unit.length();
-	
-	std::cout << v3unit.toString() << std::endl;
-	std::cout << length << std::endl;
-	
-	MFR::Vector v4 = v / 3;
-	std::cout << v.toString() << std::endl;
-	std::cout << v4.toString() << std::endl;
 
+	int width = 640;
+	int height = 480;
+
+	/* RENDER IMAGE TO SCREEN */
+	
 	//Start SDL
 	if (SDL_Init(SDL_INIT_VIDEO) != 0){
 		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
@@ -37,7 +120,7 @@ int main(int argc, const char * argv[]) {
 	}
 	
 	//Set up screen
-	SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+	SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, width, height, SDL_WINDOW_SHOWN);
 	if (win == nullptr){
 		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
 		SDL_Quit();
@@ -52,27 +135,33 @@ int main(int argc, const char * argv[]) {
 		return 1;
 	}
 	
-	SDL_Surface *bmp = SDL_LoadBMP("hello_world.bmp");
-	if (bmp == nullptr){
-		SDL_DestroyRenderer(ren);
-		SDL_DestroyWindow(win);
-		std::cout << "SDL_LoadBMP Error: " << SDL_GetError() << std::endl;
-		SDL_Quit();
-		return 1;
-	}
+	//initialize pixel buffer
+	MFR::Color * pixels = new MFR::Color[width*height];
+	calculateImage(pixels, width, height);
 	
-	SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, bmp);
-	SDL_FreeSurface(bmp);
-	if (tex == nullptr){
-		SDL_DestroyRenderer(ren);
-		SDL_DestroyWindow(win);
-		std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << std::endl;
-		SDL_Quit();
-		return 1;
-	}
+	writeImageToFile(pixels, (char *)"test.png", width, height);
 	
-	//A sleepy rendering loop, wait for 3 seconds and render and present the screen each time
-	for (int i = 0; i < 3; ++i){
+	SDL_Texture * tex = getBlankTexture(win, ren);
+	writeColorBufferToTexture(tex,pixels);
+	
+	//Main loop flag
+	bool quit = false;
+	
+	//Event handler
+	SDL_Event e;
+	
+	while(!quit)
+	{
+		//Handle events on queue
+		while( SDL_PollEvent( &e ) != 0 )
+		{
+			//User requests quit
+			if( e.type == SDL_QUIT )
+			{
+				quit = true;
+			}
+		}
+			
 		//First clear the renderer
 		SDL_RenderClear(ren);
 		//Draw the texture
@@ -81,6 +170,7 @@ int main(int argc, const char * argv[]) {
 		SDL_RenderPresent(ren);
 		//Take a quick break after all that hard work
 		SDL_Delay(1000);
+		
 	}
 	SDL_DestroyTexture(tex);
 	SDL_DestroyRenderer(ren);
@@ -94,9 +184,6 @@ int main(int argc, const char * argv[]) {
 }
 
 /*TODO:
-Figure out how to display images when the program is run and write image to file
-	- sdl?? http://www.lazyfoo.net/SDL_tutorials/lesson01/mac/xcode/index.php
-	- write to file how???
 	
 Figure out how to load scenes in order to get camera, light, material, and mesh info
 And what format to use?
